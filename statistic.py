b6c0a3b66c6ac25731fda43883562a65c7dfc63f
Tao.z <lizouzt@gmail.com>
2015-04-08 00:20:31 +0800

    update

--- statistic.py
+++ statistic.py
@@ -25,7 +25,7 @@ class Statistic(object):
     _count_insert = {}
     _count_error = {}
     _count_delete = {}
-    _count_repeat = 0
+    _count_repeat = {}
     _count_invalid_msg = 0
     def __init__(self, file_name='dht.stat'):
         self._stat_file = file_name
@@ -53,6 +53,7 @@ class Statistic(object):
 
         total_success = 0
         total_error = 0
+	total_repeat = 0
         individualData = ''
         for host in self._count_insert:
             total_success += self._count_insert[host]
@@ -63,7 +64,9 @@ class Statistic(object):
         for host in self._count_delete:
             total_error += self._count_delete[host]
             individualData += '  Host %s Delete: %d \n' % (host,self._count_delete[host])
-
+	for host in self._count_repeat:
+            total_repeat += self._count_repeat[host]
+            individualData += '  Host %s Repeat: %d \n' % (host,self._count_repeat[host])
         content = ['torrents:']
         interval = time.time() - self.begin_time
         content.append('  PID: %s \n' % os.getpid())
@@ -71,7 +74,7 @@ class Statistic(object):
         content.append('  Run time: %s \n' % get_time_formatter(interval))
         content.append('  Get invalid TCP nums: %d \n' % self._count_invalid_msg)
         content.append('  Get BT nums: %d \n' % total_success)
-        content.append('  Repeat nums: %d \n' % self._count_repeat)
+        content.append('  Repeat nums: %d \n' % total_repeat)
         content.append('  Error nums: %d \n' % total_error)
         content.append(individualData)
         content.append('\n')
@@ -113,13 +116,15 @@ class Statistic(object):
         if t is None:
             return -1
         elif t == '0':
-            print 'insert from ',dic[0]
             if dic[0] not in self._count_insert:
                 self._count_insert[dic[0]] = 1
             else:
                 self._count_insert[dic[0]] += 1
         elif t == '-1':
-            self._count_repeat += 1
+	    if dic[0] not in self._count_repeat:
+		self._count_repeat[dic[0]] = 1
+	    else:
+		self._count_repeat[dic[0]] += 1
         elif t == '1':
             if dic[0] not in self._count_error:
                 self._count_error[dic[0]] = 1
